8c84e79d6a20e674ee8f42603ea9e2d4eeb040dc
Pabloarro <pablo.arroyuelos@opendeusto.es>
2022-10-23 21:04:49 +0200

    ventanaStockV2

--- src/Ventanas/VentanaStock.java
+++ src/Ventanas/VentanaStock.java
@@ -1,13 +1,19 @@
 package Ventanas;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
 import java.awt.BorderLayout;
 
-public class VentanaStock {
-
+public class VentanaStock extends JFrame{
+	private JPanel contentPane;
 	private JFrame frame;
 
 	/**
@@ -18,34 +24,58 @@ public class VentanaStock {
 	/**
 	 * Create the application.
 	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaStock frame = new VentanaStock();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 	public VentanaStock() {
-		initialize();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelIzquierda = new JPanel();
+		contentPane.add(panelIzquierda, BorderLayout.WEST);
+		
+		JPanel panelNorte = new JPanel();
+		contentPane.add(panelNorte, BorderLayout.NORTH);
+		
+		JLabel lblStock = new JLabel("Stock");
+		lblStock.setFont(new Font("Freestyle Script", Font.BOLD, 28));
+		panelNorte.add(lblStock);
+		
+		
+		JPanel panelSur = new JPanel();
+		contentPane.add(panelSur, BorderLayout.SOUTH);
+		
+		JPanel panelDerecha = new JPanel();
+		contentPane.add(panelDerecha, BorderLayout.EAST);
+		
+		JPanel panelCentro = new JPanel();
+		contentPane.add(panelCentro, BorderLayout.CENTER);
+		JTextField txtfieldStock= new JTextField();
+		txtfieldStock.setFont(new Font("Times New Roman" , Font.BOLD, 28));
+		txtfieldStock.setBounds(10, 10, 200, 30);
+		
+		panelCentro.add(txtfieldStock);
+		
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.WEST);
-		
-		JPanel panel_1 = new JPanel();
-		frame.getContentPane().add(panel_1, BorderLayout.NORTH);
-		
-		JPanel panel_2 = new JPanel();
-		frame.getContentPane().add(panel_2, BorderLayout.SOUTH);
-		
-		JPanel panel_3 = new JPanel();
-		frame.getContentPane().add(panel_3, BorderLayout.EAST);
-		
-		JPanel panel_4 = new JPanel();
-		frame.getContentPane().add(panel_4, BorderLayout.CENTER);
-		
-		
-	}
+	
 
 }
