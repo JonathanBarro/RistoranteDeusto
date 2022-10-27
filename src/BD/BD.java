127ccf9497108b94643b41af8f2183a3a2d69bf1
Pedro Ariznavarreta <pedro.ariznavarreta@opendeusto.es>
2022-10-27 16:15:28 +0200

    BD inicio

--- src/BD/BD.java
+++ src/BD/BD.java
@@ -3,6 +3,7 @@ package BD;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class BD {
 	/**
@@ -35,7 +36,40 @@ public class BD {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			}
 		}
+		
+		public static void crearTablas(Connection con) {
+			String sql = "CREATE TABLE IF NOT EXISTS Cliente (nombre String, apellido String, contrasena String)";
+			try {
+				Statement st = con.createStatement();
+				st.executeUpdate(sql);
+				sql = "CREATE TABLE IF NOT EXISTS Admin(nombre String, apellido String, contrasena String, idAdmin Integer, sueldo Real)";
+				st.executeUpdate(sql);
+				sql = "CREATE TABLE IF NOT EXISTS Bebida(nombre String, precio Real, id Integer, stock Integer, frio String)";
+				st.executeUpdate(sql);
+				sql = "CREATE TABLE IF NOT EXISTS Comida(nombre String, precio Real, id Integer, stock Integer)";
+				st.executeUpdate(sql);
+				sql = "CREATE TABLE IF NOT EXISTS Menu_Degustacion(id String, numProductos Integer)";
+				st.executeUpdate(sql);
+				sql = "CREATE TABLE IF NOT EXISTS Menu_EntreSemana(id String, numProductos Integer, descuentoEstudiante String)";
+				st.executeUpdate(sql);
+				sql = "CREATE TABLE IF NOT EXISTS Menu_FinDeSemana(id String, numProductos Integer, numPersonas Integer)";
+				st.executeUpdate(sql);
+				sql = "CREATE TABLE IF NOT EXISTS Menu_Infantil(id String, numProductos Integer)";
+				st.executeUpdate(sql);
+				sql = "CREATE TABLE IF NOT EXISTS Mesa(idMesa String, lugar Integer, ocupada String)";
+				st.executeUpdate(sql);
+				sql = "CREATE TABLE IF NOT EXISTS Reserva(fecha String, numeroPersonas Integer, idReserva String)";
+				st.executeUpdate(sql);
+				st.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		
+	
 	}
 	
 	
--- src/Logica/RistoranteMain.java
+++ src/Logica/RistoranteMain.java
@@ -1,8 +1,18 @@
 package Logica;
 
+import java.sql.Connection;
+
+import BD.BD;
+
 public class RistoranteMain {
 	
 	public static void main(String[] args) {
+		
+		
+		Connection con = BD.initBD("RistoranteBD.db");
+		BD.crearTablas(con);
+		
+		BD.closeBD(con);
 		
 	}
 
